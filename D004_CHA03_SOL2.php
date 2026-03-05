9095ed3af6e2e12fef4d65b8f64ad4c788498592
BEN-ESSAHRAOUI-Yassine <ybenessahraoui@gmail.com>
2026-03-05 14:28:32 +0000

    add challenge 3 in php code

---
+++ D004_CHA03_SOL2.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Debt tracker</h1>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  <h2>welcome to our app !!! </h2><br> Insert name of your friend : <input type="text" name="fname">
+  <br> How much he/she owes you ? : <input type="number" name="fname2">
+  <input type="submit">
+</form>
+
+<?php
+$debt = [];
+$friend = [];
+$total = 0 ;
+
+for($i = 0; $i < 4; $i++){
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // collect value of input field
+    $friend[$i] = htmlspecialchars($_REQUEST['fname']);
+    if (empty($friend[$i])) {
+        echo "Name please!!!";
+    }
+    $cup = htmlspecialchars($_REQUEST['fname2']);
+    if (empty($debt)) {
+        echo "How much he owes you!!!";
+    }
+}
+}
+for($i = 0; $i < 4; $i++){
+    $total += $debt[$i];
+}
+echo "your total amount of debts is : " . $total . " MAD.\n and friends who owes you more than 100 DH :";
+for($i = 0; $i < 4; $i++){
+    if ($debt[$i] > 100) {
+            echo $friend[$i] & " owes you " & $debt[$i] & " MAD. \n";
+    }
+}
+?> 
+
+</body>
+</html>
