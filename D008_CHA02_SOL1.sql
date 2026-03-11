da0d62ab7357b84a10e0ceb36a63a773653a408e
BEN-ESSAHRAOUI-Yassine <ybenessahraoui@gmail.com>
2026-03-11 11:50:01 +0000

    add solution day 08 challenge 02 inserting books to a existing library

---
+++ D008_CHA02_SOL1.sql
@@ -0,0 +1,10 @@
+USE library_db;
+INSERT INTO library_books (title, author, published_year, price) VALUES
+('For Bread Alone', 'Mohamed Choukri', 1973, 12.75),
+('The Sand Child', 'Tahar Ben Jelloun', 1985, 15.30),
+('Le Passé Simple', 'Driss Chraibi', 1954, 14.50),
+('This Blinding Absence of Light', 'Tahar Ben Jelloun', 2001, 16.40),
+('Dreams of Trespass: Tales of a Harem Girlhood', 'Fatima Mernissi', 2001, 19.90),
+("The Moor's Account", 'Laila Lalami', 2014, 13.00),
+('The Voices of Marrakech', 'Elias Canetti', 1968, 11.20),
+('La Boîte à Merveilles', 'Ahmed Sefrioui', 1950, NULL);
